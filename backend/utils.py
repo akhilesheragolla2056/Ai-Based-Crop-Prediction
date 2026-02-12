89c409187a6ab0426afd9b8ce8092850e898ed66
Eragolla Akhilesh <akhilesheragolla2056@gmail.com>
2026-02-12 21:45:17 +0530

    hello

--- backend/utils.py
+++ backend/utils.py
@@ -7,7 +7,7 @@ import sys
 import os
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-if SRC_PATH not in sys.path:
+if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
 
 from functools import lru_cache
